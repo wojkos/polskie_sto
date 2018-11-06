4fa928fa733fba21ea1c181689d9712bbb11aeea
wojtek <wojtek.kostanski@gmail.com>
2018-11-07 00:32:36 +0100

    add gmail authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'login', to: redirect('/auth/google_oauth2'), as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
   resources :places
-  root to: 'pages#home'
   get 'pages/home'
-  get '*path', to: redirect('/')
+  get 'me', to: 'user#show', as: 'me'
   #TODO add somethig too display on rails notes command
+  root to: 'pages#home'
+  get '*path', to: redirect('/')
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_215318) do
+ActiveRecord::Schema.define(version: 2018_11_06_215227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,18 @@ ActiveRecord::Schema.define(version: 2018_11_05_215318) do
     t.string "title"
     t.text "description"
     t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "picture"
+    t.string "roles"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
